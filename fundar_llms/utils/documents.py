e5bea8c9b39a05cb7d3503b9fd635e0148a57967
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-10-21 19:26:12 -0300

    Modularizo utils

--- fundar_llms/utils/documents.py
+++ fundar_llms/utils/documents.py
@@ -1,144 +1,18 @@
-from tqdm.contrib.concurrent import process_map
 from langchain.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from collections.abc import Iterable, Sized
 from typing import Optional
-import uuid
-import random
-import collections
-from itertools import islice
-from copy import copy
-from types import GeneratorType
-import inspect
-from functools import wraps, reduce
+from functools import reduce
 from operator import or_ as union
-from typing import TypeVar, Sequence, Callable
-from pydantic import Field
-import subprocess
-import torch
+from typing import TypeVar, Callable
+from .uuid import UuidGenerator
+from .common import allow_opaque_constructor
+from .cuda import is_cuda_available
+import inspect
+
 
 T = TypeVar('T')
-
-def flatten(nested_list: Sequence[Sequence[T]], max_level=5) -> Sequence[T]:
-    return _flatten(nested_list, max_level)
-
-def _flatten(nested_list: Sequence[T | Sequence[T]], current_level) -> Sequence[T]:
-    if current_level == 0:
-        return nested_list
-    else:
-        return _flatten([item 
-                         for sublist in nested_list 
-                         for item in sublist 
-                         if isinstance(item, Iterable)]
-                         , current_level-1)
-
-def split_list_into_chunks(lst: list[T], chunk_size: int):
-    return [lst[i:j] for i, j in 
-            ((i, i + chunk_size) 
-                for i in range(0, len(lst), chunk_size))]
-
-def pass_generator_as_copy(*xs):
-    def wrapper(f):
-        sig = inspect.signature(f)
-
-        assert all(x in sig.parameters.keys() for x in xs)
-
-        @wraps(f)
-        def _(*args, **kwargs):
-            bound_args = sig.bind(*args, **kwargs)
-            bound_args.apply_defaults()
-            bound_args = bound_args.arguments
-            
-            for k,v in bound_args.items():
-                if not k in xs:
-                    continue
-                if not isinstance(v, GeneratorType):
-                    continue
-                bound_args[k] = list(v)
-            return f(**bound_args)
-        return _
-    return wrapper
-
-def allow_opaque_constructor(**objects):
-    def wrapper(f):
-        sig = inspect.signature(f)
-
-        assert all(x in sig.parameters.keys() for x in objects)
-
-        @wraps(f)
-        def _(*args, **kwargs):
-            bound_args = sig.bind(*args, **kwargs)
-            bound_args.apply_defaults()
-            bound_args = bound_args.arguments
-            
-            for k,v in bound_args.items():
-                if k not in objects:
-                    continue
-                if not isinstance(v, dict):
-                    continue
-                bound_args[k] = objects[k](**v)
-            return f(**bound_args)
-        return _
-    return wrapper
-
-def consume(iterator, n=None):
-    "Advance the iterator n-steps ahead. If n is none, consume entirely."
-    if n is None:
-        collections.deque(iterator, maxlen=0)
-    else:
-        next(islice(iterator, n, n), None)
-
-_is_cuda_available_ = None
-def is_cuda_available() -> bool:
-    global _is_cuda_available_
-    if _is_cuda_available_ is None:
-        from torch.cuda import is_available as is_cuda_available_
-        _is_cuda_available_ = is_cuda_available_
-    return _is_cuda_available_()
-
-class UuidGenerator:
-    def __init__(self, seed: Optional[int] = None):
-        self.seed = seed
-        self._instantiate_random()
-
-    def _instantiate_random(self):
-        self.random = random.Random()
-
-        if self.seed is not None:
-            self.random.seed(self.seed)
-
-        return self
-    
-    def reset(self):
-        return self._instantiate_random()
-
-    def _next(self):
-        return uuid.UUID(int=self.random.getrandbits(128), version=4)
-
-    def next(self, n: Optional[int] = None):
-        if n is None:
-            return self._next()
-        else:
-            return (self._next() for _ in range(n))
-    
-    def __iter__(self):
-        while True:
-            yield self.next()
-
-    @pass_generator_as_copy('xs')
-    def map(self, xs: Iterable[T], offset: Optional[int] = None) -> Iterable[uuid.UUID]: 
-        if offset is not None:
-            self.reset()
-
-            if offset > 0:
-                consume(self.next(offset))
-        
-        return map(lambda _: self.next(), xs)
-
-    @pass_generator_as_copy('xs')
-    def zipWith(self, xs: Iterable[T], offset: Optional[int] = None) -> Iterable[tuple[uuid.UUID, T]]:
-        return zip(self.map(xs, offset=offset), xs)
 
 DEFAULT_PDF_LOADER = PyPDFLoader
 
@@ -221,7 +95,7 @@ def SentenceTransformer(*args, **kwargs):
     """
     global _sentence_transformer_obj
     if _sentence_transformer_obj is None:
-        from sentence_transformers import SentenceTransformer
+        from sentence_transformers import SentenceTransformer # type: ignore
         _sentence_transformer_obj = SentenceTransformer
 
     sig = inspect.signature(_sentence_transformer_obj)
@@ -341,60 +215,3 @@ def vectorize_document(
         assert len(all_ids) == len(set(all_ids)), "IDs were not unique. Please try again with a new seed."
 
         return result
-    
-# VRAM
-
-def available_vram_nvidia_smi():
-    try:
-        result = subprocess.run(
-            ['nvidia-smi', '--query-gpu=memory.total,memory.free', '--format=csv,noheader,nounits'],
-            stdout=subprocess.PIPE,
-            encoding='utf-8'
-        )
-        output = result.stdout.strip()
-        total_mem, free_mem = map(int, output.split(','))
-        return free_mem * 1024 * 1024, total_mem * 1024 * 1024  # Convert MB to bytes
-    except FileNotFoundError:
-        pass
-
-    raise RuntimeError("nvidia-smi not found. Ensure NVIDIA drivers are installed.")
-
-def available_vram_torch():
-    if torch.cuda.is_available():
-        torch.cuda.synchronize()
-        total_mem = torch.cuda.get_device_properties(0).total_memory
-        allocated_mem = torch.cuda.memory_allocated(0)  # Memory currently allocated by tensors
-        reserved_mem = torch.cuda.memory_reserved(0)  # Memory reserved by the CUDA allocator
-        free_mem = reserved_mem - allocated_mem  # Available memory from reserved pool
-        return free_mem, total_mem
-    else:
-        raise RuntimeError("CUDA is not available.")
-    
-def get_available_vram():
-    try:
-        return available_vram_nvidia_smi()
-    except Exception as ex:
-        pass
-    
-    try:
-        return available_vram_torch()
-    except Exception as ex:
-        pass
-    
-    raise RuntimeError("Coudlnt get available memory.")
-
-
-# Models
-
-def splitmodel(x) -> tuple[str, str]:
-    if ':' not in x:
-        return x, ''
-    
-    model, specs = x.split(':')
-    return model, specs
-
-def modelname(x):
-    return splitmodel(x)[0]
-
-def modelspecs(x):
-    return splitmodel(x)[1]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages # type: ignore
 
 setup(
-    version = '1.0.3',
+    version = '1.0.4',
     name='fundar_llms',
     author='Fundar',
     description="LLM utilities created for Fundar's dev projects.",
